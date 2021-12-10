56fbe2fdfd4b74f8655f23166aea5ff87c826be9
AndresPadillaUcros <you@example.comaandres.padilla.u@outlook.com>
2021-12-09 19:40:14 -0500

    checkpoint before auth0 with graphql

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import conectarBD from './db/db.js';
 import {tipos} from './graphql/tipos.js'
 import {resolvers } from './graphql/resolvers.js'
 
+import jwt from 'express-jwt';
+import jwks from 'jwks-rsa';
 
 dotenv.config();
 
@@ -19,7 +21,19 @@ const app= express();
 app.use(express.json());
 app.use(cors());
 
+var jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://innova-mintic.us.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'innova-mintic-api',
+  issuer: 'https://innova-mintic.us.auth0.com/',
+  algorithms: ['RS256']
+});
 
+app.use(jwtCheck);
 
 app.listen({port:process.env.PORT || 4000},async()=>{
     await conectarBD();
