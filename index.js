cab70507f299af9c71e6864064fc3f3b36b6f591
JesusRestrepo <jesusrestrepo2003@gmail.com>
2021-12-10 11:26:46 -0500

    acomodando auth0 para backend

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ import conectarBD from './db/db.js';
 import {tipos} from './graphql/tipos.js'
 import {resolvers } from './graphql/resolvers.js'
 
+import jwt from 'express-jwt'
+import jwks from 'jwks-rsa'
+
 
 dotenv.config();
 
@@ -18,6 +21,38 @@ const app= express();
 
 app.use(express.json());
 app.use(cors());
+
+var jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://innova-mintic.us.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'innova-mintic-api',
+  issuer: 'https://innova-mintic.us.auth0.com/',
+  algorithms: ['RS256']
+});
+
+app.get('/inicio', jwtCheck, (req, res) => {
+    res.send('Secured Resource');
+});
+
+/*app.get('/usuarios/self' , jwtCheck, (req, res, next) => {
+    console.log('alguien hizo get en la ruta /self');
+    const token = req.headers.authorization.split('Bearer')[1];
+    const user =  jwt_decode(token)['http://localhost/userData'];
+    console.log(user);
+    baseDeDatos.collection('usuario').findOne({email: user.email}, async (err, response) => {
+      if(response){
+        console.log('response consulta base de datos', response);
+        res.json(response);
+      }
+      else{
+        next();
+      }
+    })
+  });*/
 
 
 
