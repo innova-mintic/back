f009c465cdaa884b4204d3b9196facf78d20e9df
JesusRestrepo <jesusrestrepo2003@gmail.com>
2021-12-10 11:34:07 -0500

    empleando ruta para autenticacion

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ import dotenv from 'dotenv';
 import conectarBD from './db/db.js';
 import {tipos} from './graphql/tipos.js'
 import {resolvers } from './graphql/resolvers.js'
-
 import jwt from 'express-jwt';
 import jwks from 'jwks-rsa';
 
@@ -22,36 +21,36 @@ app.use(express.json());
 app.use(cors());
 
 var jwtCheck = jwt({
-    secret: jwks.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: 'https://innova-mintic.us.auth0.com/.well-known/jwks.json'
-  }),
-  audience: 'innova-mintic-api',
-  issuer: 'https://innova-mintic.us.auth0.com/',
-  algorithms: ['RS256']
+  secret: jwks.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: 'https://innova-mintic.us.auth0.com/.well-known/jwks.json'
+}),
+audience: 'innova-mintic-api',
+issuer: 'https://innova-mintic.us.auth0.com/',
+algorithms: ['RS256']
 });
 
 app.get('/inicio', jwtCheck, (req, res) => {
-    res.send('Secured Resource');
+  res.send('Secured Resource');
 });
 
-/*app.get('/usuarios/self' , jwtCheck, (req, res, next) => {
-    console.log('alguien hizo get en la ruta /self');
-    const token = req.headers.authorization.split('Bearer')[1];
-    const user =  jwt_decode(token)['http://localhost/userData'];
-    console.log(user);
-    baseDeDatos.collection('usuario').findOne({email: user.email}, async (err, response) => {
-      if(response){
-        console.log('response consulta base de datos', response);
-        res.json(response);
-      }
-      else{
-        next();
-      }
-    })
-  });*/
+app.get('/usuarios/self' , jwtCheck, (req, res, next) => {
+  console.log('alguien hizo get en la ruta /self');
+  const token = req.headers.authorization.split('Bearer')[1];
+  const user =  jwt_decode(token)['http://localhost/userData'];
+  console.log(user);
+  baseDeDatos.collection('usuario').findOne({email: user.email}, async (err, response) => {
+    if(response){
+      console.log('response consulta base de datos', response);
+      res.json(response);
+    }
+    else{
+      next();
+    }
+  })
+});
 
 
 app.listen({port:process.env.PORT || 4000},async()=>{
